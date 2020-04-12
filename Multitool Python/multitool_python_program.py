524cba39a3661520ce2355eb918a2c3c860750e2
Paulina Polewiak <paulina.polewiak98@gmail.com>
2020-04-12 14:58:40 +0200

    Adding files

--- Multitool Python/multitool_python_program.py
+++ Multitool Python/multitool_python_program.py
@@ -1,62 +1,82 @@
+from moduly.calculators import calculatorCtoF
+from moduly.calculators import calculatorFtoC
+from moduly.calculators import pole_kola
+from moduly.binarne import binarytodecimal
+from moduly.calculators import rokprzestepny
+from moduly.drawnings import rectangle
+from moduly.drawnings import piramida
+from moduly.numbers import pierwsza_ostatnia_cyfra
+from moduly.numbers import podzielnaprzez3lub5lub7
+from moduly.numbers import podzielnaprzez3i5i7
+from moduly.calculators import wiekpsa
+from moduly.numbers import parzysta
+import random
 
-print('Witaj w Multitool Python Program!')
-wybor = None
-while wybor is None:
-    try:
-        print(
-        '''Wybierz numer programu, który Cię interesuje:
-            1) Kalkulator stopni Celsjusza na Fahrenheita
-            2) Kalkulator stopni Fahrenheita na Celsjusza
-            3) Obliczenie pola powierzchni koła o zadanym promieniu
-            4) Przeliczanie liczby zapisanej w formacie binarnym na system dziesiętny (w budowie ;))
-            5) Program do sprawdzania czy podany rok jest rokiem przestępnym
-            6) Program, który rysuje prostokąt o zadanych rozmiarach
-            7) Program rysujący piramidę o określonej wysokości
-            8) Program, który podaje pierwszą i ostatnią cyfrę podanej liczby
-            9) Program do rozpoznawania czy podana liczba jest parzysta czy nie
-            10) Program do sprawdzania czy liczba jest podzielna przez 3 lub 5 lub 7
-            11) Program do sprawdzania czy liczba jest podzielne przez 3 i 5 i 7
-            R) Wybierz program losowo
-            X) Wyjście z programu''')
-        wybor = int(input("Twój wybór: "))
-        if wybor == 1:
-            from moduly.calculators import calculatorCtoF
-            calculatorCtoF()
-        elif wybor == 2:
-            from moduly.calculators import calculatorFtoC
-            calculatorFtoC()
-        elif wybor == 3:
-            from moduly.calculators import pole_kola
-            pole_kola()
-        elif wybor == 4:
-            from moduly.binarne import binarytodecimal
-            binarytodecimal()
-        elif wybor == 5:
-            from moduly.calculators import rokprzestepny
-        elif wybor == 6:
-            from moduly.drawnings import rectangle
-            rectangle()
-        elif wybor == 7:
-            from moduly.drawnings import piramida
-            piramida()
-        elif wybor == 8:
-            from moduly.numbers import pierwsza_ostatnia_cyfra
-            pierwsza_ostatnia_cyfra()
-        elif wybor == 9:
-            from moduly.numbers import parzysta
-            parzysta()
-        elif wybor == 10:
-            from moduly.numbers import podzielnaprzez3lub5lub7
-            podzielnaprzez3lub5lub7()
-        elif wybor == 11:
-            from moduly.numbers import podzielnaprzez3i5i7
-            podzielnaprzez3i5i7()
-        elif wybor == 12:
-            from moduly.calculators import wiekpsa
-            wiekpsa()
-        elif wybor == 13:
-            print("Mam nadzieję, że jeszcze wrócisz! Miłego dnia :)")
-            break
-    except:
-        print("Wpisane dane są nieprawidłowe. Spróbuj jeszcze raz.")
+# print('Witaj w Multitool Python Program!'
+#       '\n Wybierz program, który Cię interesuje'
+#       '\n - 1) Kalkulator stopni Celcjusza na Fahrenheita'
+#       '\n - 2) Kalkulator stopni Fahrenheita na Celcjusza'
+#       '\n - 3) Obliczanie pola powierzchni koła o zadanym promieniu'
+#       '\n - 4) Przeliczanie liczby zapisanej w formacie binarnym na system dziesiętny (w trakcie tworzenia)'
+#       '\n - 5) Program do sprawdzania czy podany rok jest rokiem przestępnym'
+#       '\n - 6) Program, który rysuje prostokąt o zadanych rozmiarach'
+#       '\n - 7) Program rysujący piramidę o określonej wysokości'
+#       '\n - 8) Program, który podaje pierwszą i ostatnią cyfrę podanej liczby'
+#       '\n - 9) Program do rozpoznawania czy podana liczba jest parzysta czy nie'
+#       '\n - 10) Program do sprawdzania czy liczba jest podzielna przez 3 lub 5 lub 7'
+#       '\n - 11) Program do sprawdzania czy liczba jest podzielna przez 3 i 5 i 7'
+#       '\n - R) Wybierz program losowo'
+#       '\n - X) Wyjście z programu')
+# program = input("Co chcesz zrobić? Twój wybór: ")
 
+
+def multitool():
+    print('Witaj w Multitool Python Program!'
+              '\n Wybierz program, który Cię interesuje'
+              '\n - 1) Kalkulator stopni Celcjusza na Fahrenheita'
+              '\n - 2) Kalkulator stopni Fahrenheita na Celcjusza'
+              '\n - 3) Obliczanie pola powierzchni koła o zadanym promieniu'
+              '\n - 4) Przeliczanie liczby zapisanej w formacie binarnym na system dziesiętny (w trakcie tworzenia)'
+              '\n - 5) Program do sprawdzania czy podany rok jest rokiem przestępnym'
+              '\n - 6) Program, który rysuje prostokąt o zadanych rozmiarach'
+              '\n - 7) Program rysujący piramidę o określonej wysokości'
+              '\n - 8) Program, który podaje pierwszą i ostatnią cyfrę podanej liczby'
+              '\n - 9) Program do rozpoznawania czy podana liczba jest parzysta czy nie'
+              '\n - 10) Program do sprawdzania czy liczba jest podzielna przez 3 lub 5 lub 7'
+              '\n - 11) Program do sprawdzania czy liczba jest podzielna przez 3 i 5 i 7'
+              '\n - 12) Program do obliczania wieku psa'
+              '\n - 13) Wybierz program losowo'
+              '\n - 0) Wyjście z programu')
+    program = int(input("Co chcesz zrobić? Twój wybór: "))
+    funkcje = ['', calculatorCtoF, calculatorFtoC, pole_kola, binarytodecimal,
+               rokprzestepny, rectangle, piramida, pierwsza_ostatnia_cyfra, parzysta,
+               podzielnaprzez3lub5lub7, podzielnaprzez3i5i7, wiekpsa]
+    if program == 1:
+        funkcje[program]()
+    elif program == 2:
+        funkcje[program]()
+    elif program == 3:
+        funkcje[program]()
+    elif program == 4:
+        funkcje[program]()
+    elif program == 5:
+        funkcje[program]()
+    elif program == 6:
+        funkcje[program]()
+    elif program == 7:
+        funkcje[program]()
+    elif program == 8:
+        funkcje[program]()
+    elif program == 9:
+        funkcje[program]()
+    elif program == 10:
+        funkcje[program]()
+    elif program == 11:
+        funkcje[program]()
+    elif program == 12:
+        funkcje[program]()
+    elif program == 13:
+        random.choice(funkcje)()
+    elif program == 0:
+        print("Mam nadzieję, że jeszcze wrócisz! Do zobaczenia :)")
+multitool()
